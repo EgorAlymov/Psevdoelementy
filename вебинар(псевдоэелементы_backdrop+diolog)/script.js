fdd0f950a711eb0140d734b5435bdf2c797ac4e4
EgorAlymov <168273050+EgorAlymov@users.noreply.github.com>
2024-09-11 19:00:12 +0500

    Add files via upload

---
+++ вебинар(псевдоэелементы_backdrop+diolog)/script.js
@@ -0,0 +1,9 @@
+var modal = document.querySelector('dialog');
+document.querySelector('#open').onclick = function() {
+    modal.showModal();
+}
+
+document.querySelector('#close').onclick = function() {
+    modal.close();
+}
+
